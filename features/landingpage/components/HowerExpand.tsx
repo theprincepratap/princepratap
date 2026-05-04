708ac0e1b142a17be630f86ecc15b31754eadd7f
theprincepratap <theprincepratap@gmail.com>
2026-05-04 23:50:12 +0530

    build error fixed

--- features/landingpage/components/HowerExpand.tsx
+++ features/landingpage/components/HowerExpand.tsx
@@ -7,11 +7,13 @@ const HowerExpand = () => {
       src: "/path/to/image1.jpg",
       alt: "kg section",
       code: "# 01",
+      link: "#",
     },
     {
       src: "https://i.pinimg.com/736x/37/dc/1d/37dc1d83c8e7577ebebdfb70ba0fe70b.jpg",
       alt: " class 2 to 5",
       code: "# 02",
+      link: "#",
     },
     // ... more images
   ];
